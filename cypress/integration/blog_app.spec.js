703f64cbc40f08a56ae5d246e8f011f5b4af8966
Anton Taleiko <anton.taleiko@gmail.com>
2021-04-09 11:25:39 +0300

    5.22: Liking has been implemented. The tests still need to check that the order is correct.

--- cypress/integration/blog_app.spec.js
+++ cypress/integration/blog_app.spec.js
@@ -16,27 +16,27 @@ describe('Blog app', function() {
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
-    cy.contains('Log in to application')
-  })
+  // it('Login form is shown', function() {
+  //   cy.contains('Log in to application')
+  // })
 
-  describe('Login',function() {
-    it('succeeds with correct credentials', function() {
-      cy.get('#username').type('h4m5t3r')
-      cy.get('#password').type('hemligt')
-      cy.get('#login-button').click()
+  // describe('Login',function() {
+  //   it('succeeds with correct credentials', function() {
+  //     cy.get('#username').type('h4m5t3r')
+  //     cy.get('#password').type('hemligt')
+  //     cy.get('#login-button').click()
 
-      cy.contains('The Hamster logged in')
-    })
+  //     cy.contains('The Hamster logged in')
+  //   })
 
-    it('fails with wrong credentials', function() {
-      cy.get('#username').type('hamster')
-      cy.get('#password').type('heml')
-      cy.get('#login-button').click()
+  //   it('fails with wrong credentials', function() {
+  //     cy.get('#username').type('hamster')
+  //     cy.get('#password').type('heml')
+  //     cy.get('#login-button').click()
 
-      cy.contains('wrong username or password')
-    })
-  })
+  //     cy.contains('wrong username or password')
+  //   })
+  // })
 
   describe('When logged in', function() {
     beforeEach(function() {
@@ -45,36 +45,79 @@ describe('Blog app', function() {
       cy.get('#login-button').click()
     })
 
-    it('A blog can be created and liked and deleted', function() {
+    // it('A blog can be created and liked and deleted', function() {
+    //   cy.get('#toggle-button').click()
+    //   cy.get('#title').type('This is a test')
+    //   cy.get('#author').type('testbot')
+    //   cy.get('#url').type('www.testbot.com')
+    //   cy.get('#submit-blog-button').click()
+
+    //   cy.contains('This is a test by testbot added')
+    //   cy.contains('This is a test testbot')
+    //   cy.contains('view').click()
+    //   cy.contains('www.testbot.com')
+    //   cy.contains('likes 0')
+
+    //   cy.contains('like').click()
+    //   cy.contains('likes 1')
+
+    //   cy.get('#logout-button').click()
+    //   cy.get('#username').type('user2')
+    //   cy.get('#password').type('letmein')
+    //   cy.get('#login-button').click()
+    //   cy.contains('remove').should('not.exist')
+
+    //   cy.get('#logout-button').click()
+    //   cy.get('#username').type('h4m5t3r')
+    //   cy.get('#password').type('hemligt')
+    //   cy.get('#login-button').click()
+    //   cy.contains('view').click()
+    //   cy.contains('remove').click()
+    //   cy.contains('This is a test testbot')
+    //     .should('not.exist')
+    // })
+
+    it('Blogs are ordered according to likes', function() {
       cy.get('#toggle-button').click()
-      cy.get('#title').type('This is a test')
+      cy.get('#title').type('urgh')
       cy.get('#author').type('testbot')
       cy.get('#url').type('www.testbot.com')
       cy.get('#submit-blog-button').click()
 
-      cy.contains('This is a test by testbot added')
-      cy.contains('This is a test testbot')
-      cy.contains('view').click()
-      cy.contains('www.testbot.com')
-      cy.contains('likes 0')
+      cy.get('#toggle-button').click()
+      cy.get('#title').type('good')
+      cy.get('#author').type('testbot')
+      cy.get('#url').type('www.testbot.com')
+      cy.get('#submit-blog-button').click()
 
-      cy.contains('like').click()
-      cy.contains('likes 1')
+      cy.get('#toggle-button').click()
+      cy.get('#title').type('favorite')
+      cy.get('#author').type('testbot')
+      cy.get('#url').type('www.testbot.com')
+      cy.get('#submit-blog-button').click()
 
-      cy.get('#logout-button').click()
-      cy.get('#username').type('user2')
-      cy.get('#password').type('letmein')
-      cy.get('#login-button').click()
-      cy.contains('remove').should('not.exist')
+      cy.contains('good')
+        .parent()
+        .contains('view')
+        .click()
+      cy.contains('good')
+        .parent()
+        .contains('like')
+        .click()
 
-      cy.get('#logout-button').click()
-      cy.get('#username').type('h4m5t3r')
-      cy.get('#password').type('hemligt')
-      cy.get('#login-button').click()
-      cy.contains('view').click()
-      cy.contains('remove').click()
-      cy.contains('This is a test testbot')
-        .should('not.exist')
+      cy.contains('favorite')
+        .parent()
+        .contains('view')
+        .click()
+      cy.contains('favorite')
+        .parent()
+        .contains('like')
+        .click()
+      cy.wait(2000)
+      cy.contains('favorite')
+        .parent()
+        .contains('like')
+        .click()
     })
   })
 })
